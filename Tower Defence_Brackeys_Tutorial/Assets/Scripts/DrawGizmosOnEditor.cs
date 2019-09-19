c27b237abeb9600f9dac52f78033fe040bea12ad
JojuGeorge <jojugeorge001@gmail.com>
2019-09-19 13:32:51 +0530

    Enemy AI
    Waypoint.cs added Waypoints gameobj
    
    DrawGizmosOnEditor.cs added to Waypoints gameojb to draw line between waypoints.
    
    EnemyMovement.cs script added to enemy prefab to move along the waypoints in the path to the end and then destroy

---
+++ Tower Defence_Brackeys_Tutorial/Assets/Scripts/DrawGizmosOnEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawGizmosOnEditor : MonoBehaviour
+{
+    [SerializeField] private Transform[] _points;
+
+    private void Start()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_points != null && _points.Length >= 1)
+        {
+            for (int i = 0; i < _points.Length; i++)
+            {
+                Gizmos.DrawLine(_points[i].transform.position,
+                                _points[i + 1 > _points.Length - 1 ? i : i + 1].transform.position);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(_points[i].transform.position, .5f);
+
+            }
+        }
+    }
+}
---
+++ Tower Defence_Brackeys_Tutorial/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+
+    private Transform _target;
+    private int _waypointIndex = 0;
+
+    private void Start()
+    {
+        _target = Waypoint.points[_waypointIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = _target.position - transform.position;
+        transform.Translate(dir.normalized * _speed * Time.deltaTime, Space.World);
+
+        // If we've reached the next waypoint then
+        if (Vector3.Distance(transform.position, _target.position) < 0.2f)
+        {
+            GetNextWaypoint();
+        }
+
+    }
+
+    // Gets the next waypoint
+    private void GetNextWaypoint()
+    {
+        _waypointIndex++;
+        if(_waypointIndex < Waypoint.points.Length)
+        {
+            _target = Waypoint.points[_waypointIndex];
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;                 // Destroy() might have a slight delay
+        }
+    }
+}
