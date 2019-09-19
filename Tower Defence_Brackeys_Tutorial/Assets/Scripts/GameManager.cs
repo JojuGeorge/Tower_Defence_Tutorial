b0b0bca7f60bb6f237813ab8693f8dab65ec4f5d
JojuGeorge <jojugeorge001@gmail.com>
2019-09-19 19:34:03 +0530

    Enemy Wave - use of singleton pattern in GameManager.cs
    Working Enemy wave.
    UI for showing wave countdown. Used UIManager.cs

---
+++ Tower Defence_Brackeys_Tutorial/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform _enemyPrefab;
+    [SerializeField] private Transform _enemySpawnPoint;
+    [SerializeField] private float _timeBetweenWaves = 5f;
+    [SerializeField] private float _delayBetweenEnemySpawn = .1f;         // Delay of each enemies in a spawn group
+
+
+    private int _waveNumber = 0;
+
+    private float _countDown = 2f;
+    public float CountDown { get { return _countDown; } set { _countDown = value; } }
+
+    // Singleton
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    
+    private void Awake()
+    {
+        // Needs this inorder for proper working of singleton
+        if(Instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+
+    private void Update()
+    {
+        CountDown -= Time.deltaTime;
+
+        if(CountDown <= 0)
+        {
+            SpawnWave();
+            CountDown = _timeBetweenWaves;
+        }
+    }
+
+    private void SpawnWave()
+    {
+        _waveNumber++;
+       StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        for (int i = 0; i < _waveNumber; i++)
+        {
+            Instantiate(_enemyPrefab, _enemySpawnPoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(_delayBetweenEnemySpawn);
+        }
+    }
+}
---
+++ Tower Defence_Brackeys_Tutorial/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text _countDownTimerText;
+
+
+    void Update()
+    {
+        float countDown = Mathf.Round(GameManager.Instance.CountDown);
+        _countDownTimerText.text = countDown.ToString();
+    }
+}
