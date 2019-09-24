cdee605155e394695d0709eedc20dbd6a801a399
JojuGeorge <jojugeorge001@gmail.com>
2019-09-24 13:49:45 +0530

    Level select basic functionality added
    Level Select scene added.
    LevelSelect.cs added.
    Scrolling of levels and press level 1 or 2 to go to level 1 or 2

---
+++ Tower Defence_Brackeys_Tutorial/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField] private SceneFader _sceneFader;
+
+    public void Select(string levelToLoad)
+    {
+        if (levelToLoad != "")
+        {
+            _sceneFader.FadeTo(levelToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("No level is set to open on click");
+        }
+    }
+}
